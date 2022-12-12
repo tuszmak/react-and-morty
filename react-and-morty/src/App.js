c8611dc89525a72c22066bcb6121aba4ea516ef5
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2022-12-12 14:47:15 +0100

    feat: skeleton

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import { useCharacters, useLocations } from "./api/useData";
+import Card from "./components/Card";
+
 
 function App() {
   const characters = useCharacters(1);
@@ -11,8 +13,16 @@ function App() {
   console.log("Locations data: ");
   console.log(locations);
 
-  return <div className="App">Take a look at the console! (F12)
-  <img src="./starTrekLogo.svg"></img></div>;
+  return (
+    <div className="App">
+      Take a look at the console! (F12)
+      <img src="Rick_and_Morty.svg" />
+      <button>Show Characters</button>
+      <button>Show Locations</button>
+      <Card />
+
+    </div>
+  );
 }
 
 export default App;
