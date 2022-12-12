8d44c7a6f238378acb521c4c0b8dc926b3fabb43
akoshm <mogyorosi98@gmailcom>
2022-12-12 14:58:05 +0100

    feat: Landing page w required functionality

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./App.css";
 import { useCharacters, useLocations } from "./api/useData";
+import Landing from "./components/Landing";
 
 function App() {
   const characters = useCharacters(1);
   const locations = useLocations(1);
-
   console.log("Characters data: ");
   console.log(characters);
   console.log("Locations data: ");
   console.log(locations);
 
-  return <div className="App">Take a look at the console! (F12)</div>;
+  return <>
+    <Landing />
+  </>
 }
 
 export default App;
