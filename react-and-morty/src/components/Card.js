a0e01987c9c4f102f1c10001b173cf0bc852322f
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2022-12-13 10:59:41 +0100

    feat: name is now a link

--- react-and-morty/src/components/Card.js
+++ react-and-morty/src/components/Card.js
@@ -2,14 +2,16 @@ import { useState } from "react";
 import {Link, link} from "react-router-dom"
 
 export default function Card({character}) {
+    function handleExtension(){
+        setIsExtended(!isExtended)
+    }
     const [isExtended, setIsExtended] = useState(false); //when you extend the card to show more info about it.
 
     return (<ul>
-        <li>{character.name}</li>
+        <li><a onClick={handleExtension}>{character.name}</a></li>
        {/* <Link to={character.location.url}> <li>{character.location.name}</li></Link> */}
        <li>{character.location.name}</li>
        
-        {/* if(isExtended) */}
 
     </ul>)
 }
