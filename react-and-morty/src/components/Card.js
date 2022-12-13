ead667d0868af2cc02a652953a2ca633bc5872dc
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2022-12-13 12:00:57 +0100

    fix: the whole character section is a link now

--- react-and-morty/src/components/Card.js
+++ react-and-morty/src/components/Card.js
@@ -5,27 +5,26 @@ export default function Card({ character }) {
     function handleExtension() {
         setIsExtended(!isExtended)
     }
-    const [isExtended, setIsExtended] = useState(true); //when you extend the card to show more info about it.
+    const [isExtended, setIsExtended] = useState(false); // boolean if a card info is extended
 
     return (
         isExtended ?
             <ul>
-                <li><a onClick={handleExtension}>{character.name}</a></li>
-
-                <li>{character.location.name}</li>
+                <a onClick={handleExtension}><li>Name: {character.name}</li>
+                <li>Location: {character.location.name}</li>
                 {/* <Link to={character.location.url}> <li>{character.location.name}</li></Link> */}
                 <li>Status: {character.status}</li>
                 <li>Species: {character.species}</li>
                 <li>Episode Count:  {character.episode.length}</li>
                 <li>Origin: {character.origin.name}</li>
-                <li>Gender: {character.gender}</li>
+                <li>Gender: {character.gender}</li></a>
 
             </ul>
             : <ul>
-            <li><a onClick={handleExtension}>{character.name}</a></li>
+            <a onClick={handleExtension}><li>Name: {character.name}</li>
 
-            <li>{character.location.name}</li>
-            {/* <Link to={character.location.url}> <li>{character.location.name}</li></Link> */}
+            <li>Location: {character.location.name}</li>
+            {/* <Link to={character.location.url}> <li>{character.location.name}</li></Link> */}</a>
             
 
         </ul>)
