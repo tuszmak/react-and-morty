afcefbdfd018c1bb097ccf39946539cfceecea4b
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2022-12-13 11:30:55 +0100

    fix: fixed merge

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -1,30 +1,24 @@
-import useFetch from "./api/useFetch";
 import "./App.css";
-import { useCharacters, useLocations } from "./api/useData";
+// import { useCharacters, useLocations } from "./api/useData";
 import Header from "./components/Header";
+import useFetch from "./api/useFetch";
 
 function App() {
- const { isPending, error, data } = useFetch('https://rickandmortyapi.com/api/character/?page=')
+  const { isPending, error, data } = useFetch('https://rickandmortyapi.com/api/character/?page=')
   console.log(data, isPending, error);
-  const characters = useCharacters(1);
-  const locations = useLocations(1);
-  console.log("Characters data: ");
-  console.log(characters);
-  console.log("Locations data: ");
-  console.log(locations);
+  // const characters = useCharacters(1);
+  // const locations = useLocations(1);
+  // console.log("Characters data: ");
+  // console.log(characters);
+  // console.log("Locations data: ");
+  // console.log(locations);
 
-  return <>
-    
-    <div className="App">
+  return (<div className="App">
     {isPending && <div>loading...</div>}
     {error && <div>{error}</div>}
     {data && <div>data</div>}
     <Header />
 
-  </div>
-  </>
+  </div>)}
 
-
-
-
-export default App;
+  export default App;
