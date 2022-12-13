d04a2b908a00d743e93004da8bdadccd3518ffd3
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2022-12-13 11:52:00 +0100

    feat: extended character info added

--- react-and-morty/src/components/Card.js
+++ react-and-morty/src/components/Card.js
@@ -1,17 +1,35 @@
 import { useState } from "react";
-import {Link, link} from "react-router-dom"
+import { Link, link } from "react-router-dom"
 
-export default function Card({character}) {
-    function handleExtension(){
+export default function Card({ character }) {
+    function handleExtension() {
         setIsExtended(!isExtended)
     }
-    const [isExtended, setIsExtended] = useState(false); //when you extend the card to show more info about it.
+    const [isExtended, setIsExtended] = useState(true); //when you extend the card to show more info about it.
 
-    return (<ul>
-        <li><a onClick={handleExtension}>{character.name}</a></li>
-       {/* <Link to={character.location.url}> <li>{character.location.name}</li></Link> */}
-       <li>{character.location.name}</li>
+    return (
+        isExtended ?
+            <ul>
+                <li><a onClick={handleExtension}>{character.name}</a></li>
+
+                <li>{character.location.name}</li>
+                {/* <Link to={character.location.url}> <li>{character.location.name}</li></Link> */}
+                <li>Status: {character.status}</li>
+                <li>Species: {character.species}</li>
+                <li>Episode Count:  {character.episode.length}</li>
+                <li>Origin: {character.origin.name}</li>
+                <li>Gender: {character.gender}</li>
+
+            </ul>
+            : <ul>
+            <li><a onClick={handleExtension}>{character.name}</a></li>
+
+            <li>{character.location.name}</li>
+            {/* <Link to={character.location.url}> <li>{character.location.name}</li></Link> */}
+            
+
+        </ul>)
+}
+       
        
 
-    </ul>)
-}
