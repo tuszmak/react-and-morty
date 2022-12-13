1fc270d5a3fd489ff01baec5150478433ae2f94a
MateWagner <wwagnermate@gmail.com>
2022-12-13 17:24:53 +0100

    feat: location.js  done and prep for character.js

--- react-and-morty/src/components/Location.js
+++ react-and-morty/src/components/Location.js
@@ -1,8 +1,47 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import useFetch from "../api/useFetch";
+import { mainUrls } from '../api/dataRoutes'
 
 const Location = () => {
   const { id } = useParams()
-  return (<div>Prop: {id}</div>);
+  const { isPending, error, data: loc } = useFetch(`${mainUrls.locations}${id}`)
+  console.log(loc);
+  return (
+    <div>
+      {isPending && <h1>Loading...</h1>}
+      {error && <h1>{error}</h1>}
+      {loc && <div>
+        <h1>{loc.name}</h1>
+        <h3>{loc.type}</h3>
+        <p>Dimension:{loc.dimension}</p>
+        <h2>Residents:</h2>
+        <ul>
+          <ResidentsList residents={loc.residents} />
+        </ul>
+      </div>}
+    </div>
+  )
 }
 
 export default Location;
+
+
+const ResidentsList = ({ residents }) => {
+  const maxTen = Math.min(residents.length, 10);
+  const list = []
+  for (let i = 0; i < maxTen; i++) {
+    list.push(<li key={i}>{<Resident url={residents[i]} />}</li>)
+  }
+  return list
+}
+const Resident = ({ url }) => {
+  const { isPending, error, data } = useFetch(url)
+  console.log(data);
+  return (
+    <>
+      {isPending && <h1>Loading...</h1>}
+      {error && <h1>{error}</h1>}
+      {data && <p>Name: <Link to={`/character/${data.id}`}>{data.name}</Link></p>}
+    </>
+  )
+}
