8dd0bb5a53bbc64a0c57b44db504e6dc74cff161
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2022-12-13 13:05:44 +0100

    feat: location ID function is separate

--- react-and-morty/src/components/Card.js
+++ react-and-morty/src/components/Card.js
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import { Link } from "react-router-dom"
+import getLocationID from "../utils/getLocationID";
 
 export default function Card({ character }) {
     const [isExtended, setIsExtended] = useState(false); //when you extend the card to show more info about it.
     return (<ul>
         <li>{character.name}</li>
-        <li><Link to={`/location/${character.location.url.split('/').at(-1)}`}>{character.location.name}</Link></li>
-        {/* <li>{character.location.name}</li> */}
-
-        {/* if(isExtended) */}
+        <li><Link to={`/location/${getLocationID(character.location.url)}`}>{character.location.name}</Link></li>
 
     </ul>)
 }
