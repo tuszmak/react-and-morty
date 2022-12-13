56c57cf6c5307db8eb10d0b8e9483c9f14371ca3
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2022-12-13 08:57:22 +0100

    fix: changes

--- react-and-morty/src/components/Card.js
+++ react-and-morty/src/components/Card.js
@@ -1,90 +1,33 @@
-import { useState } from "react"
+import { useState } from "react";
 
-let a = {
-    "id": 1,
-    "name": "Rick Sanchez",
-    "status": "Alive",
-    "species": "Human",
-    "type": "",
-    "gender": "Male",
-    "origin": {
-        "name": "Earth (C-137)",
-        "url": "https://rickandmortyapi.com/api/location/1"
+let a = [
+    {
+        id: 1,
+        name: "Rick Sanchez",
+        location: {
+            name: "Citadel of Ricks",
+            url: "https://rickandmortyapi.com/api/location/3",
+        },
     },
-    "location": {
-        "name": "Citadel of Ricks",
-        "url": "https://rickandmortyapi.com/api/location/3"
+    {
+        location: {
+            name: "Citadel of Ricks",
+            url: "https://rickandmortyapi.com/api/location/3",
+        },
+        name: "Morty Smith",
     },
-    "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    "episode": [
-        "https://rickandmortyapi.com/api/episode/1",
-        "https://rickandmortyapi.com/api/episode/2",
-        "https://rickandmortyapi.com/api/episode/3",
-        "https://rickandmortyapi.com/api/episode/4",
-        "https://rickandmortyapi.com/api/episode/5",
-        "https://rickandmortyapi.com/api/episode/6",
-        "https://rickandmortyapi.com/api/episode/7",
-        "https://rickandmortyapi.com/api/episode/8",
-        "https://rickandmortyapi.com/api/episode/9",
-        "https://rickandmortyapi.com/api/episode/10",
-        "https://rickandmortyapi.com/api/episode/11",
-        "https://rickandmortyapi.com/api/episode/12",
-        "https://rickandmortyapi.com/api/episode/13",
-        "https://rickandmortyapi.com/api/episode/14",
-        "https://rickandmortyapi.com/api/episode/15",
-        "https://rickandmortyapi.com/api/episode/16",
-        "https://rickandmortyapi.com/api/episode/17",
-        "https://rickandmortyapi.com/api/episode/18",
-        "https://rickandmortyapi.com/api/episode/19",
-        "https://rickandmortyapi.com/api/episode/20",
-        "https://rickandmortyapi.com/api/episode/21",
-        "https://rickandmortyapi.com/api/episode/22",
-        "https://rickandmortyapi.com/api/episode/23",
-        "https://rickandmortyapi.com/api/episode/24",
-        "https://rickandmortyapi.com/api/episode/25",
-        "https://rickandmortyapi.com/api/episode/26",
-        "https://rickandmortyapi.com/api/episode/27",
-        "https://rickandmortyapi.com/api/episode/28",
-        "https://rickandmortyapi.com/api/episode/29",
-        "https://rickandmortyapi.com/api/episode/30",
-        "https://rickandmortyapi.com/api/episode/31",
-        "https://rickandmortyapi.com/api/episode/32",
-        "https://rickandmortyapi.com/api/episode/33",
-        "https://rickandmortyapi.com/api/episode/34",
-        "https://rickandmortyapi.com/api/episode/35",
-        "https://rickandmortyapi.com/api/episode/36",
-        "https://rickandmortyapi.com/api/episode/37",
-        "https://rickandmortyapi.com/api/episode/38",
-        "https://rickandmortyapi.com/api/episode/39",
-        "https://rickandmortyapi.com/api/episode/40",
-        "https://rickandmortyapi.com/api/episode/41",
-        "https://rickandmortyapi.com/api/episode/42",
-        "https://rickandmortyapi.com/api/episode/43",
-        "https://rickandmortyapi.com/api/episode/44",
-        "https://rickandmortyapi.com/api/episode/45",
-        "https://rickandmortyapi.com/api/episode/46",
-        "https://rickandmortyapi.com/api/episode/47",
-        "https://rickandmortyapi.com/api/episode/48",
-        "https://rickandmortyapi.com/api/episode/49",
-        "https://rickandmortyapi.com/api/episode/50",
-        "https://rickandmortyapi.com/api/episode/51"
-    ],
-    "url": "https://rickandmortyapi.com/api/character/1",
-    "created": "2017-11-04T18:48:46.250Z"
-}
-let objectString = '';
+];
+let objectString = "";
 let objectArray = Object.entries(a);
 
-
-export default function Card(character){
+export default function Card(character) {
     const [isExtended, setIsExtended] = useState(false);
-    return(
+    return (
         <div className="card">
             <ul>
-                
-                <li>{a.name}</li>
-                <li>{a.location.name}</li>
+                <li>{a[0].name}</li>
+                <li>{a[0].location.name}</li>
             </ul>
         </div>
-    )
+    );
 }
