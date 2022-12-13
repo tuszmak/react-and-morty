fab049f6da41b2db6bf15968157c1c108a4ac562
MateWagner <wwagnermate@gmail.com>
2022-12-13 12:51:18 +0100

    fix: url

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -4,6 +4,7 @@ import CharacterList from './components/CharacterList'
 import { Routes, Switch, Route } from 'react-router-dom'
 import Home from "./components/Home";
 import Location from "./components/Location";
+import { mainUrls } from "./api/dataRoutes";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/characters" element={<CharacterList url={'https://rickandmortyapi.com/api/character/?page='} />} />
+        <Route path="/characters" element={<CharacterList url={mainUrls.characters} />} />
         <Route path="/location/:id" element={<Location />} />
       </Routes>
     </div>
