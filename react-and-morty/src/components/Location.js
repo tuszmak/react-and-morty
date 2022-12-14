bf61011a9345dd163a46489d0fedddfe0fb5377c
MateWagner <wwagnermate@gmail.com>
2022-12-14 13:14:49 +0100

    feat: integrated style from app.css

--- react-and-morty/src/components/Location.js
+++ react-and-morty/src/components/Location.js
@@ -1,26 +1,41 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import useFetch from "../api/useFetch";
-import { mainUrls } from '../api/dataRoutes'
-import "./location.css"
+import { mainUrls } from "../api/dataRoutes";
 import ResidentsList from "./ResidentList";
-const Location = () => {
-  const { id } = useParams()
-  const { isPending, error, data: loc } = useFetch(`${mainUrls.locations}${id}`)
-  console.log(loc);
+import { useState } from "react";
+
+const Location = ({ locationID }) => {
+  let { id } = useParams();
+  const fetchId = !id ? locationID : id;
+  const [isExtended, setIsExtended] = useState(!id ? false : true);
+
+  function handleExtension() {
+    setIsExtended(!isExtended);
+  }
+  const { isPending, error, data: loc } = useFetch(`${mainUrls.locations}${fetchId}`);
+
+
   return (
     <div>
       {isPending && <h1>Loading...</h1>}
       {error && <h1>{error}</h1>}
-      {loc && <div className="loc-content center">
-        <h1>{loc.name}</h1>
-        <h3>Type: {loc.type}</h3>
-        <p>Dimension: {loc.dimension}</p>
-        <h2>Residents:</h2>
-        <ul>
-          <ResidentsList residents={loc.residents} />
-        </ul>
-        <p>More Later...</p>
-      </div>}
+      {loc && (
+        <div className="card-container center">
+          <h1>{loc.name}</h1>
+          <h3>Type: {loc.type}</h3>
+          <p>Dimension: {loc.dimension}</p>
+          {isExtended && (
+            <>
+              {" "}
+              <h2>Residents:</h2>
+              <ul>
+                <ResidentsList residents={loc.residents} />
+              </ul>
+            </>
+          )}
+          {!id && <button onClick={handleExtension}>{isExtended ? "Collapse" : "Extend"}</button>}
+        </div>
+      )}
     </div>
   );
 };
