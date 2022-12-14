6f094d285e5b78a02958f0a1db870fa33e95aafd
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2022-12-14 12:00:15 +0100

    feat: start of extendButton

--- react-and-morty/src/components/CharacterList.js
+++ react-and-morty/src/components/CharacterList.js
@@ -3,18 +3,27 @@ import useFetch from "../api/useFetch";
 import Card from "./Card";
 
 export default function CharacterList({ url }) {
-    const { isPending, error, data } = useFetch(url)
+    
+    const [loadedPageNumber, setLoadedPageNumber] = useState(1);
+    function handleExtendPage(){
+        setLoadedPageNumber(loadedPageNumber++);
+    }
     console.log(data);
-    return (
+    for (let i = 1; i <= loadedPageNumber; i++) {
+        const { isPending, error, data } = useFetch(url + `?page=${loadedPageNumber}`)
+        return (
 
-        <div className="character-list" style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
-            {isPending && <div>loading...</div>}
-            {error && <div>{error}</div>}
-            {data && data.results.map((character) =>
-                <Card key={character.id} character={character} />
-            )}
-
-
-        </div>
-    );
+            <div className="character-list" style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+                {isPending && <div>loading...</div>}
+                {error && <div>{error}</div>}
+                {data && data.results.map((character) =>
+                    <Card key={character.id} character={character} />
+                )}
+                <button></button>
+    
+    
+            </div>
+        );
+    }
+    
 }
