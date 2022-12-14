4df0314d5c199144f53bc3670f354ce28040d3be
MateWagner <wwagnermate@gmail.com>
2022-12-14 14:29:54 +0100

    only one character detail page

--- react-and-morty/src/components/Card.js
+++ react-and-morty/src/components/Card.js
@@ -2,31 +2,30 @@ import { useState } from "react";
 import getLocationID from "../utils/getLocationID";
 import { Link } from "react-router-dom";
 
-export default function Card({ character }) {
-    const [isExtended, setIsExtended] = useState(false); // boolean if a card info is extended
+export default function Card({ character, isOnlyOne }) {
+    const [isExtended, setIsExtended] = useState(isOnlyOne ? true : false); // boolean if a card info is extended
     function handleExtension() {
         setIsExtended(!isExtended);
     }
     return (
-        <div class="card-container" style={{}}>
+        <div class={`card-container ${isOnlyOne && 'center'}`} style={{}}>
             <ul>
                 <img src={character.image} alt="charImage" ></img>
                 <DefaultInfo character={character} />
                 {isExtended && <ExpandedInfo character={character} />}
             </ul>
-            <div class="cardBtn-container" >
-                <button class="cardBtn" onClick={handleExtension}>{isExtended ? "▲" : "▼"}</button>
-            </div>
+            {!isOnlyOne &&
+                <div class="cardBtn-container" >
+                    <button class="cardBtn" onClick={handleExtension}>{isExtended ? "▲" : "▼"}</button>
+                </div>}
         </div >
     )
 }
 
 function DefaultInfo({ character }) {
-    console.log(character);
     return <>
-        <li>Name: {character.name}</li>
-        <li>Location: <Link to={`/location/${getLocationID(character.location.url)}`}>{character.location.name}</Link>
-        </li>
+        <li>Name: <Link to={`/character/${character.id}`}> {character.name}</Link></li>
+        <li>Location: <Link to={`/location/${getLocationID(character.location.url)}`}>{character.location.name}</Link></li>
     </>
 }
 function ExpandedInfo({ character }) {
--- react-and-morty/src/components/Character.js
+++ react-and-morty/src/components/Character.js
@@ -1,12 +1,22 @@
 import { useParams } from "react-router-dom";
+import useFetch from "../api/useFetch";
+import { mainUrls } from "../api/dataRoutes";
+import Card from "./Card";
 
 const Character = () => {
   const { id } = useParams()
+  const { isPending, error, data: character } = useFetch(`${mainUrls.characters}${id}`);
   return (
     <div>
-      <h1>id:{id}</h1>
+      {isPending && <h1>Loading...</h1>}
+      {error && <h1>{error}</h1>}
+      {character && (
+        <div>
+          <Card character={character} isOnlyOne={true} />
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
 export default Character;
