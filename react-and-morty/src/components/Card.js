5076e01d325f4ae9c0831f896c33fe781c130de1
akoshm <mogyorosi98@gmailcom>
2022-12-14 10:11:46 +0100

    fix: Optimised Card.js & added temporary css

--- react-and-morty/src/components/Card.js
+++ react-and-morty/src/components/Card.js
@@ -3,48 +3,39 @@ import getLocationID from "../utils/getLocationID";
 import { Link } from "react-router-dom"
 
 export default function Card({ character }) {
+    const [isExtended, setIsExtended] = useState(false); // boolean if a card info is extended
     function handleExtension() {
         setIsExtended(!isExtended)
     }
-    function DefaultInfo() {
-        return <>
-            <li>Name: {character.name}</li>
-            <li>Location: <Link to={`/location/${getLocationID(character.location.url)}`}>{character.location.name}</Link>
-            </li>
-        </>
-    }
-    function ExpandedInfo() {
-        return <>
-            <li>Status: {character.status}</li>
-            <li>Species: {character.species}</li>
-            <li>Episode Count:{character.episode.length}</li>
-            <li>Origin: {character.origin.name}</li>
-            <li>Gender: {character.gender}</li>
-        </>
-    }
-    const [isExtended, setIsExtended] = useState(false); // boolean if a card info is extended
 
     return (
-        isExtended ?
-            <ul>
-                <div>
-                    <DefaultInfo />
-                    <ExpandedInfo />
-                </div>
-                <button onClick={handleExtension}>Collapse</button>
-                {/* <button class="collapsible">Rick Sanchez</button>
-                <div class="content">
-                    <div>
-                       
-                    </div>
-                </div> */}
+        <div style={{ width: "250px " }}>
+            <ul style={{ background: "blue" }}>
+                <img src={character.image} alt="charImage" style={{ width: "200px" }}></img>
+                <DefaultInfo character={character} />
+                {isExtended && <ExpandedInfo character={character} />}
             </ul>
-            :
-            <ul>
-                <div>
-                    <DefaultInfo />
-                </div>
-                <button onClick={handleExtension}>Expand</button>
-            </ul>
+            <div style={{ background: "green" }}>
+                <button onClick={handleExtension}>{isExtended ? "Collapse" : "Extend"}</button>
+            </div>
+        </div>
     )
+}
+
+function DefaultInfo({ character }) {
+    console.log(character);
+    return <>
+        <li>Name: {character.name}</li>
+        <li>Location: <Link to={`/location/${getLocationID(character.location.url)}`}>{character.location.name}</Link>
+        </li>
+    </>
+}
+function ExpandedInfo({ character }) {
+    return <>
+        <li>Status: {character.status}</li>
+        <li>Species: {character.species}</li>
+        <li>Episode Count:{character.episode.length}</li>
+        <li>Origin: {character.origin.name}</li>
+        <li>Gender: {character.gender}</li>
+    </>
 }
