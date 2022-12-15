c3e861a31a2cd8740de09fca8019e9452d843fb6
MateWagner <wwagnermate@gmail.com>
2022-12-15 08:57:21 +0100

    feat: character is in the own component don't use the card

--- react-and-morty/src/components/Character.js
+++ react-and-morty/src/components/Character.js
@@ -1,21 +1,29 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import useFetch from "../api/useFetch";
 import { mainUrls } from "../api/dataRoutes";
-import Card from "./Card";
+import getLocationID from "../utils/getLocationID";
 
 const Character = () => {
   const { id } = useParams()
   const { isPending, error, data: character } = useFetch(`${mainUrls.characters}${id}`);
   return (
-    <div>
+    <div className={'card-container center'}>
       {isPending && <h1>Loading...</h1>}
       {error && <h1>{error}</h1>}
-      {character && (
-        <div>
-          <Card character={character} isOnlyOne={true} />
-        </div>
-      )}
-    </div>
+      {character &&
+        <>
+          <img src={character.image} alt="charImage" ></img>
+          <ul>
+            <li>Name: <Link to={`/character/${character.id}`}> {character.name}</Link></li>
+            <li>Location: <Link to={`/location/${getLocationID(character.location.url)}`}>{character.location.name}</Link></li>
+            <li>Status: {character.status}</li>
+            <li>Species: {character.species}</li>
+            <li>Episode Count:{character.episode.length}</li>
+            <li>Origin: {character.origin.name}</li>
+            <li>Gender: {character.gender}</li>
+          </ul>
+        </>}
+    </div >
   )
 }
 
